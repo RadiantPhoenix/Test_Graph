d3bf8af399103023bff54324af4a38540d1b0aff
RadiantPhoenix <vladislav.kniazev@gmail.com>
2020-02-13 12:03:31 +0300

    Added weight support

--- src/main/java/DirectedGraph.java
+++ src/main/java/DirectedGraph.java
@@ -10,22 +10,42 @@ public class DirectedGraph extends Graph {
         try {
             Vertex v1 = new Vertex(label1);
             Vertex v2 = new Vertex(label2);
-            verticesArray.get(v1).add(v2);
+            WeightedEdge w = new WeightedEdge(v2);
+            verticesArray.get(v1).add(w);
         } catch (Exception e) {
             throw new Exception("Label " + label1 + " or " + label2 + " not found");
         }
     }
 
     @Override
-    public void removeEdge(String label1, String label2) throws Exception  {
+    public void addEdge(String label1, String label2, int weight) throws Exception {
         try {
             Vertex v1 = new Vertex(label1);
             Vertex v2 = new Vertex(label2);
-            List<Vertex> eV1 = verticesArray.get(v1);
-            if (eV1 != null)
-                eV1.remove(v2);
+            WeightedEdge w = new WeightedEdge(v2, weight);
+            verticesArray.get(v1).add(w);
         } catch (Exception e) {
             throw new Exception("Label " + label1 + " or " + label2 + " not found");
         }
     }
+
+
+    @Override
+    public void removeEdge(String label1, String label2) throws Exception  {
+        try {
+            Vertex v1 = new Vertex(label1);
+            Vertex v2 = new Vertex(label2);
+            List<WeightedEdge> eV1 = verticesArray.get(v1);
+
+            for (WeightedEdge w : eV1) {
+                if (v2.equals(w.getVertex())) {
+                    eV1.remove(w);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new Exception("Label " + label1 + " or " + label2 + " not found");
+        }
+    }
+
 }
